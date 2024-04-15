65e3231a820eb400e99cda95b4ea70e66042cf0b
Nikita Yarosh <yarosh.nv@yandex.ru>
2024-04-15 18:01:32 +0300

    feature: доработка контроллера

--- src/main/java/org/manager/crazy/task/tracker/api/controllers/ProjectController.java
+++ src/main/java/org/manager/crazy/task/tracker/api/controllers/ProjectController.java
@@ -3,16 +3,21 @@ package org.manager.crazy.task.tracker.api.controllers;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.manager.crazy.task.tracker.api.dto.AckDto;
 import org.manager.crazy.task.tracker.api.dto.ProjectDto;
 import org.manager.crazy.task.tracker.api.exceptions.BadRequestException;
+import org.manager.crazy.task.tracker.api.exceptions.NotFoundException;
 import org.manager.crazy.task.tracker.api.factories.ProjectDtoFactory;
 import org.manager.crazy.task.tracker.store.entities.ProjectEntity;
 import org.manager.crazy.task.tracker.store.repositories.ProjectRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -24,20 +29,85 @@ public class ProjectController {
 
     ProjectDtoFactory projectDtoFactory;
 
+    public static final String FETCH_PROJECTS = "/api/projects";
     public static final String CREATE_PROJECT = "/api/projects";
+    public static final String EDIT_PROJECT = "/api/projects{project_id}";
+    public static final String DELETE_PROJECT = "/api/projects{project_id}";
+
+
+    @GetMapping(FETCH_PROJECTS)
+    public List<ProjectDto> fetchProjects(
+            @RequestParam(value = "prefix_name", required = false)Optional<String> optionalPrefixName) {
+
+        optionalPrefixName = optionalPrefixName.filter(prefixName -> !prefixName.trim().isEmpty());
+
+        Stream<ProjectEntity> projectStream = optionalPrefixName
+                .map(projectRepository::streamAllByNameStartsWithIgnoreCase)
+                .orElseGet(projectRepository::streamAll);
+
+        return projectStream
+                .map(projectDtoFactory::makeProjectDto)
+                .collect(Collectors.toList());
+    }
 
     @PostMapping(CREATE_PROJECT)
     public ProjectDto createProject(
-            @RequestParam String name
-    ){
+            @RequestParam String name) {
+        if(name.trim().isEmpty()){
+            throw new BadRequestException("Name can't be empty.");
+        }
+
+
         projectRepository
                 .findByName(name)
                 .ifPresent(project -> {
                     throw new BadRequestException(String.format("Project \"%s\" already exists.", name));
                 });
 
-        //TODO: uncommitted and insert entity in method
-        //return  projectDtoFactory.makeProjectDto();
-        return null;
+        ProjectEntity project = projectRepository.saveAndFlush(
+                ProjectEntity.builder()
+                        .name(name)
+                        .build()
+        );
+
+        return projectDtoFactory.makeProjectDto(project);
+    }
+
+    @PatchMapping(EDIT_PROJECT)
+    public ProjectDto editProject(
+            @PathVariable("project_id") Long projectId,
+            @RequestParam String name) {
+        ProjectEntity project = projectRepository
+                .findById(projectId)
+                .orElseThrow(() -> new NotFoundException(
+                        String.format("Project with \"%s\" doesn't exist.", projectId))
+                );
+
+        projectRepository
+                .findByName(name)
+                .filter(anotherProject -> !Objects.equals(anotherProject.getId(), projectId))
+                .ifPresent(anotherProject -> {
+                    throw new BadRequestException(String.format("Project \"%s\" already exists.", name));
+                });
+
+        project.setName(name);
+
+        project = projectRepository.saveAndFlush(project);
+
+        return projectDtoFactory.makeProjectDto(project);
+    }
+
+    @DeleteMapping(DELETE_PROJECT)
+    public AckDto deleteProject(@PathVariable("project_id") Long projectId){
+
+        projectRepository
+                .findById(projectId)
+                .orElseThrow(() -> new NotFoundException(
+                        String.format("Project with \"%s\" doesn't exist.", projectId))
+                );
+
+        projectRepository.deleteById(projectId);
+
+        return AckDto.makeDefault(true);
     }
 }
--- src/main/java/org/manager/crazy/task/tracker/store/repositories/ProjectRepository.java
+++ src/main/java/org/manager/crazy/task/tracker/store/repositories/ProjectRepository.java
@@ -4,8 +4,14 @@ import org.manager.crazy.task.tracker.store.entities.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
 
     Optional<ProjectEntity> findByName(String name);
+
+    Stream<ProjectEntity> streamAll();
+    Stream<ProjectEntity> streamAllByNameStartsWithIgnoreCase(String prefixName);
+
+
 }
