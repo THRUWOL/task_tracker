0052beb2a1cfe89aae3f2ba4f07530faf073b613
Nikita Yarosh <yarosh.nv@yandex.ru>
2024-04-15 13:11:18 +0300

    feature: формированы entity

---
+++ src/main/java/org/manager/crazy/task/tracker/store/entities/ProjectEntity.java
@@ -0,0 +1,32 @@
+package org.manager.crazy.task.tracker.store.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "project")
+public class ProjectEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Builder.Default
+    private Instant createdAt = Instant.now();
+
+    @Builder.Default
+    @OneToMany
+    private List<TaskStateEntity> taskStates = new ArrayList<>();
+}
---
+++ src/main/java/org/manager/crazy/task/tracker/store/entities/TaskStateEntity.java
@@ -0,0 +1,33 @@
+package org.manager.crazy.task.tracker.store.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "task_state")
+public class TaskStateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private Long ordinal;
+
+    @Builder.Default
+    private Instant createdAt = Instant.now();
+
+    @Builder.Default
+    @OneToMany
+    private List<TaskEntity> tasks = new ArrayList<>();
+}
